e7c51786d3e1f0e47839e0d65551de7782e37421
przi <piotr.ziembicki@gft.com>
2018-07-11 12:11:08 +0200

    PubSubController added

---
+++ main/gc-pub-sub-end-point/src/main/java/pubsub/Event.java
@@ -0,0 +1,22 @@
+package pubsub;
+
+import java.io.Serializable;
+
+public class Event implements Serializable {
+  private String message;
+
+  public Event() {
+  }
+
+  public Event(String message) {
+    this.message = message;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+}
