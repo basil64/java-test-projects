6d851f7437090ac6cd2eefda32a1f6586d09c9d5
Piotr Ziembicki <pzmz@poczta.onet.pl>
2018-07-14 13:06:37 +0200

    Bajki robotow added.

--- main/src/main/java/lem/Lem.java
+++ main/src/main/java/lem/Lem.java
@@ -4,4 +4,5 @@ public static void main(String[] argv) {
    System.out.println("This is Lem application.");
    System.out.println("Lem's books are the following:");
    System.out.println("   Cyberiada");
+   System.out.println("   Bajki robotow");
 }
