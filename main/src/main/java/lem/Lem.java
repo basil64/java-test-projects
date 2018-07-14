7acd3dfd46585d6f7398c559e4bf96109a8015ee
Piotr Ziembicki <pzmz@poczta.onet.pl>
2018-07-14 13:00:44 +0200

    Cyberiada added

--- main/src/main/java/lem/Lem.java
+++ main/src/main/java/lem/Lem.java
@@ -3,4 +3,5 @@ package lem;
 public static void main(String[] argv) {
    System.out.println("This is Lem application.");
    System.out.println("Lem's books are the following:");
+   System.out.println("   Cyberiada");
 }
